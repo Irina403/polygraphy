b4c43a5154f775b6f451a7391dff6fb1473f6e02
Irina403 <92080770+Irina403@users.noreply.github.com>
2021-11-04 15:28:09 +0300

    1

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -300,3 +300,47 @@ var appContent = (function() {
     //appPopup.init();
   });
 }(jQuery));
+
+
+$('.popup-open').click(function(e) {
+  e.preventDefault()
+  target = '#' + $(this).attr('data-id')
+  // console.log(target)
+$(target).fadeIn(function () {
+      // $(this).children( ".popup" ).css("display", "block").animate({ opacity: 1 }, 198);
+// return false;
+  })
+});	
+
+// Клик по ссылке "Закрыть".
+$('.popup-close').click(function() {
+$(this).parents('.popup-fade').fadeOut();
+return false;
+});        
+
+// Закрытие по клавише Esc.
+$(document).keydown(function(e) {
+if (e.keyCode === 27) {
+e.stopPropagation();
+$('.popup-fade').fadeOut();
+}
+});
+
+// Клик по фону, но не по окну.
+$('.popup-fade').click(function(e) {
+if ($(e.target).closest('.popup').length == 0) {
+$(this).fadeOut();					
+}
+
+});
+
+$(function () {
+  $(".submenu").hide();
+  $(".parent").click(function (e) {
+      e.preventDefault();
+      if (!$(e.target).closest("ul").is(".submenu")) {
+          $(".submenu", this).toggle();
+          $(this).siblings(".parent").find(".submenu").hide();
+      }
+  });
+});
